3a6d6de2d2a773105e3901da1122fc5df24afb09
krakenyyc <carcln@me.com>
2021-02-27 15:40:02 -0700

    hopeful changes lol

---
+++ src/RedBox.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const RedBox = () => {
+  return (
+    <div className="redbox">
+      <h1>I'm in the RedBox</h1>
+    </div>
+  );
+};
+
+export default RedBox;
