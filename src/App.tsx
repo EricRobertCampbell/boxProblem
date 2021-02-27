95ac96f2d6474f069fa3d11ad3349da5d6f6ac99
EricRobertCampbell <eric.robert.campbell@gmail.com>
2021-02-27 16:24:20 -0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import './App.css';
-import StartBtn from './components/StartBtn';
-import AddBoxBtn from './components/AddBoxBtn';
-
-
-function App() {
-  return (
-    <div>
-      <StartBtn />
-      <AddBoxBtn />
-    </div>
-  );
-=======
-
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
@@ -22,20 +6,24 @@ import BallCounter from "./components/BallCounter";
 import BlueBox from "./components/BlueBox";
 import RedBoxList from "./components/RedBoxList";
 import RedBox from "./components/RedBox";
-import InputBox from './components/InputBox/InputBox'
+import InputBox from "./components/InputBox/InputBox";
+import StartBtn from "./components/StartBtn";
+import AddBoxBtn from "./components/AddBoxBtn";
+import CNRedBox from "./RedBox";
 
 function App() {
 	return (
 		<>
+			<StartBtn />
+			<AddBoxBtn />
 			<BallCounter count={5} />
 			<BlueBox count={7} />
 			<RedBox count={9} />
 			<RedBoxList ballsList={[1, 2, 3, 4]} />
-      <InputBox />
+			<InputBox />
+			<CNRedBox />
 		</>
 	);
-
->>>>>>> bcc1057d8f9292a1ba4de14967afb0a4fb70e64a
 }
 
 export default App;
